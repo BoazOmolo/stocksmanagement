ee6af75e42170b97d687a903aef587bd9d5436e7
omolo14 <omoloboaz32@gmail.com>
2023-08-17 12:37:13 +0300

    made dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sale;
+use App\Models\Product;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('/auth/dashboard');
+        $totalsales = Sale::count();
+        $totalstock = Product::where('stockquantity', '>', 0)->count();
+        $totaloutofstock = Product::where('stockquantity', '<=', 0)->count();
+        return view('/auth/dashboard', compact('totalsales','totalstock','totaloutofstock'));
     }
 }
